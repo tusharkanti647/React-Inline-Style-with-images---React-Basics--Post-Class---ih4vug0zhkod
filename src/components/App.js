b5e4044be8619c8b606c01a86f3c4ff77c709117
tusharkanti647 <107229334+tusharkanti647@users.noreply.github.com>
2023-04-17 16:42:59 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/App.css';
+import pic from '../NewtonSchool.jpg'
+const App = () => {
+
+  return (
+    <div id="navbar">
+      <div>NavBar</div>
+      <img />
+    </div>
+  )
+}
+
+
+export default App;
